d887213151f4051e06306bcd38fbf8c8b9a6a487
손동천 <kij8025@naver.com>
2024-12-08 01:39:23 +0900

    test33

--- front/src/Auction/Component/ResultBox.jsx
+++ front/src/Auction/Component/ResultBox.jsx
@@ -6,20 +6,28 @@ const ResultBox = ({ result, option, onClick }) => {
       <div className="grid grid-cols-4 ">
         <div className="flex justify-center items-center">{option.accName}</div>
         <div>
-          <p>등급 - {result?.grade || "1212"}</p>
-          <p>이름 - {result?.name || "2121"}</p>
-          <p>품질 - {result?.gradeQuality || "3333"}</p>
-          <p>거래가능횟수 - {result?.auctionInfo?.tradeAllowCount || "33"}</p>
+          <p>등급 - {result?.grade || "결과 없음"}</p>
+          <p>이름 - {result?.name || "결과 없음"}</p>
+          <p>품질 - {result?.gradeQuality || "결과 없음"}</p>
+          <p>
+            거래가능횟수 -{" "}
+            {result?.auctionInfo?.tradeAllowCount === 0
+              ? "0"
+              : result?.auctionInfo?.tradeAllowCount || "결과 없음"}
+          </p>
         </div>
         <div>
           <p>
-            {result?.options[1]?.optionName || "111"} - {result?.options[1]?.value || "111"}
+            {result?.options[1]?.optionName || "결과 없음"} -{" "}
+            {result?.options[1]?.value || "결과 없음"}
           </p>
           <p>
-            {result?.options[2]?.optionName || "111"} - {result?.options[2]?.value || "111"}
+            {result?.options[2]?.optionName || "결과 없음"} -{" "}
+            {result?.options[2]?.value || "결과 없음"}
           </p>
           <p>
-            {result?.options[3]?.optionName || "111"} - {result?.options[3]?.value || "111"}
+            {result?.options[3]?.optionName || "결과 없음"} -{" "}
+            {result?.options[3]?.value || "결과 없음"}
           </p>
         </div>
         <div>가격 - {result?.auctionInfo?.buyPrice || "0"}</div>
