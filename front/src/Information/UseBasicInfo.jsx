a7dfc56ce7e7e57709f063df2b9ebdcdbadf576c
손동천 <kij8025@naver.com>
2024-12-09 13:58:58 +0900

    test46

--- front/src/Information/UseBasicInfo.jsx
+++ front/src/Information/UseBasicInfo.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Info from "../Assets/info.jpg";
 
 function UseBasicInfo(props) {
-  return <div className="mainbody">basic</div>;
+  return (
+    <div className="mainbody">
+      <img src={Info} alt="Info" />
+    </div>
+  );
 }
 
 export default UseBasicInfo;
